2467f3074a2ba7592e3b7e871b263378d6378ece
Dirk Van Haerenborgh <vhdirk@gmail.com>
2023-10-13 22:23:17 +0200

    fix lint errors

--- spec/download_path_spec.sh
+++ spec/download_path_spec.sh
@@ -1,15 +1,13 @@
-
 export ASDF_INSTALL_VERSION="2023.09.19"
 export ASDF_INSTALL_PATH=/tmp
 
 Describe 'asdf-bat-extras'
-  Include 'bin/install'
+Include 'bin/install'
 
-  Describe 'get_download_url()'
-    It 'returns correct URL'
-      When call get_download_url "${ASDF_INSTALL_VERSION}" 'bat-extras'
-
-      The output should equal "https://github.com/eth-p/bat-extras/releases/download/v2023.09.19/bat-extras-202309.19.zip"
-    End
-  End
+Describe 'get_download_url()'
+It 'returns correct URL'
+When call get_download_url "${ASDF_INSTALL_VERSION}" 'bat-extras'
+The output should equal "https://github.com/eth-p/bat-extras/releases/download/v2023.09.19/bat-extras-202309.19.zip"
+End
+End
 End
